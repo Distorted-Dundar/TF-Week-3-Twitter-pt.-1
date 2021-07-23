34573f5eef623294fd532cef3efdb823f353acb0
B. Flores <56740397+Distorted-Dundar@users.noreply.github.com>
2021-07-23 19:37:41 -0400

    Twitter lite pt1 week 3

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,131 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Brian Flores on 7/22/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadTweets()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+    }
+    
+    @objc func loadTweets(){
+        
+        numberOfTweet = 20
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myparams = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myparams, success: { (tweets: [NSDictionary]) in
+            
+            
+            self.tweetArray.removeAll()
+            
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+            self.myRefreshControl.endRefreshing()
+        }, failure: { (Error) in
+            print("Tweets could not load")
+        })
+    }
+    
+    
+    func loadMoreTweets() {
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = numberOfTweet + 20
+        let myparams = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myparams, success: { (tweets: [NSDictionary]) in
+            
+            
+            self.tweetArray.removeAll()
+            
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+        }, failure: { (Error) in
+            print("Tweets could not load")
+        })
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, willDisplay cellL: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+        
+        
+    }
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        
+//        Make the standard variable to switch to false since you logged out
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        // since we are getting items from tweetCekktable view we can now handpick items to go into the cells
+        
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = (tweetArray[indexPath.row]["text"] as? String)
+        
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data {
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+//        we only have 1 section
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tweetArray.count
+//        how many do you want 5 rows ex
+    }
+
+    
+
+}
